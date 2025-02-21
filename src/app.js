94047aec95a2889191e8de98d0245bbb1b6e86d1
90cris <166570889+90cris@users.noreply.github.com>
2025-02-21 17:01:17 -0300

    cambiosCors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require("express");
+require("dotenv").config();
 const routes = require("./routes/routers");
 const morgan = require("morgan");
 const cors = require("cors");
 const { errorMiddleware } = require("./middlewares/errorsManager");
 
 const app = express();
+const {CLIENT_URL} = process.env;
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors({
+
+    origin:[CLIENT_URL],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE" , "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
 
 app.use(routes);
 app.use(errorMiddleware);
